ab2ce2ea885d4e732e4121e2f7ea0c6a2796bbd0
Aloshkya <alexis.belyakoff@yandex.ru>
2025-08-18 20:28:19 +0300

    fixed issues with last update

--- run-tests.sh
+++ run-tests.sh
@@ -1,25 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
-echo "Starting tests in Docker..."
+cd /workspace
 
 # Запуск виртуального дисплея
 Xvfb :99 -screen 0 1366x768x24 & sleep 3
-export DISPLAY=:99
 
 # Старт записи экрана
 ffmpeg -y -video_size 1366x768 -framerate 15 -f x11grab -i :99 \
-    -codec:v libx264 -pix_fmt yuv420p screen_recording.mp4 > /dev/null 2>&1 & echo $! > ffmpeg_pid.txt
+  -codec:v libx264 -pix_fmt yuv420p screen_recording.mp4 > /dev/null 2>&1 & echo $! > ffmpeg_pid.txt
 
-# Запуск Maven тестов
+# Запуск тестов через Maven
 mvn -B clean test -DsuiteXmlFile='src/test/resources/StartSwap.xml' \
     -DSEED_PHRASE="$SEED" -DPASSWORD="$PASS" -DPIN="$PIN"
 
 # Остановка записи
 kill -INT $(cat ffmpeg_pid.txt) && sleep 2
 
-# Копирование видео для Allure
+# Сохраняем видео для Allure
 mkdir -p target/allure-results
 cp screen_recording.mp4 target/allure-results/
-
-echo "Tests finished!"
